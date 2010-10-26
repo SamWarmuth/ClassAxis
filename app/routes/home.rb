cb48bc337262e7ec9678f8511b619c59874316e0
Sam Warmuth <sam@samwarmuth.com>
2010-10-25 19:13:50 -0500

    initial commit

---
+++ app/routes/home.rb
@@ -0,0 +1,5 @@
+class Main
+  get "/" do
+    haml :index
+  end
+end
---
+++ init.rb
@@ -0,0 +1,32 @@
+ROOT_DIR = File.expand_path(File.dirname(__FILE__)) unless defined? ROOT_DIR
+
+require "rubygems"
+
+begin
+  require "vendor/dependencies/lib/dependencies"
+rescue LoadError
+  require "dependencies"
+end
+
+require "monk/glue"
+require "couchrest"
+require "haml"
+require "sass"
+require "json"
+
+class Main < Monk::Glue
+  set :app_file, __FILE__
+  use Rack::Session::Cookie
+end
+
+# Connect to couchdb.
+couchdb_url = monk_settings(:couchdb)[:url]
+COUCHDB_SERVER = CouchRest.database!(couchdb_url)
+
+
+# Load all application files.
+Dir[root_path("app/**/*.rb")].each do |file|
+  require file
+end
+
+Main.run! if Main.run?
