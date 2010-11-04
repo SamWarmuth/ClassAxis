90cc95ace69a7d8bd7639085b5371f3e14972b37
Kellene Buchwald <kellene.buchwald@gmail.com>
2010-11-03 21:15:26 -0500

    Added a couple properties to topic and user.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,6 +2,7 @@ class Topic < CouchRest::ExtendedDocument
   use_database COUCHDB_SERVER
 
   property :title
-  property :initial_post
+  property :initial_post_id
+  property :tags
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < CouchRest::ExtendedDocument
   use_database COUCHDB_SERVER
   
   property :name
+  property :password
   property :email
+  property :events
   
 end
