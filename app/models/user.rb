9315dfcfe757d346f98798c0e3c1795b3b1b01cc
Sam Warmuth <sam@samwarmuth.com>
2010-11-03 21:34:47 -0500

    added password hashing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,19 @@ class User < CouchRest::ExtendedDocument
   use_database COUCHDB_SERVER
   
   property :name
-  property :password
   property :email
   property :events
+  
+  def set_password(password)
+    self.salt = 64.times.map{|l|('a'..'z').to_a[rand(25)]}.join
+    self.password_hash = (Digest::SHA2.new(512) << (self.salt + password + "thyuhwdhlbajhrqmdwxgayegpjxjdomaj")).to_s
+  end
+  def valid_password?(password)
+    return false if (self.password_hash.nil? || self.salt.nil?)
+    return ((Digest::SHA2.new(512) << (self.salt + password + "thyuhwdhlbajhrqmdwxgayegpjxjdomaj")).to_s == password_hash)
+  end
+
+  property :password_hash
+  property :salt
   
 end
